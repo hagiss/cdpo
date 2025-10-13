5f28f42accaabda0a4e6c4207b2580096823be14
hagiss <wlgh410@gmail.com>
2025-10-13 12:48:33 +0900

    add sd1.5 full script

---
+++ launchers/sd15_full.sh
@@ -0,0 +1,31 @@
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export DATASET_NAME="sayakpaul/pickapic_v2_webdataset"
+
+export ADAPTER_INIT="tmp-sd15-all-scores-csft-500steps-fixlabel-mlp-ipadapter-simple-multidim/checkpoint-500"
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch train.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --dataset_name=$DATASET_NAME \
+  --train_batch_size=8 \
+  --dataloader_num_workers=16 \
+  --gradient_accumulation_steps=64 \
+  --max_train_steps 1000 \
+  --lr_scheduler="constant_with_warmup" --lr_warmup_steps=100 \
+  --learning_rate=1e-8 --scale_lr \
+  --cache_dir="/data4/mvv_full/" \
+  --checkpointing_steps 200 \
+  --beta_dpo 5000 \
+  --cdpo \
+  --multi_dim \
+  --streaming \
+  --scores_mapping_file scores_mapping_fix.pkl \
+  --cond_projector_type "mlp" \
+  --cond_mlp_hidden_dim 4096 \
+  --cond_positive_text "win win win win win" \
+  --cond_negative_text "lose lose lose lose lose" \
+  --ip_adapter_ckpt=$ADAPTER_INIT \
+  --ip_adapter \
+  --report_to="wandb" \
+  --simultaneous_conditioning \
+  --output_dir="tmp-sd15-all-scores-cdpo-1000steps-5000beta-2048batch-fixlabel-20norm-mlp-ipadapter-sfttrained-simultaneous-simple-multidim"
